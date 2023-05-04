c71808d6773e8f8dc9d8565c15065f100bee26a0
Ankush_Karande <ankush.karande@omsoftware.net>
2023-05-04 11:17:17 +0530

    initial commit

---
+++ iwttoken/src/main/java/com/java/jwt/iwttoken/IwttokenApplication.java
@@ -0,0 +1,13 @@
+package com.java.jwt.iwttoken;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IwttokenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IwttokenApplication.class, args);
+	}
+
+}
